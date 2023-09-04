f3f3062e8fece6ff40488700721e70f487684ca0
harshmons <harshverma04111989@gmail.com>
2023-09-04 19:42:14 +0530

    Initial commit

--- src/com/enablement/Helper.groovy
+++ src/com/enablement/Helper.groovy
@@ -1,16 +1,7 @@
 package com.enablement
 
-// import groovy.json.JsonSlurper
-// // def request = libraryResource 'com/devops/request.json' 
-// def slurper = new JsonSlurper()
-// def result = slurper.parseText('{"name":"Harsh Verma","age":33,"pets":["dog","cat"]}')
-// // def result = slurper.parseText(request)
-def result = [
-  name:'Harsh Verma',
-  age:23
-]
 class Helper{
   public static logger(msg){
-    return "Message from Logger helper method ${msg} ${result.name} ${result.age}"
+    return "Message from Logger helper method ${msg}"
   }
 }
