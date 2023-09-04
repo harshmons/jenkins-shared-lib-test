e682fe03fdf0dd02d9813d3ff1da918545a4808a
harshmons <harshverma04111989@gmail.com>
2023-09-04 19:48:23 +0530

    Initial commit

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,14 +1,11 @@
 import com.enablement.Helper
-// import groovy.json.JsonSlurper
-// // def request = libraryResource 'com/devops/request.json' 
-// def slurper = new JsonSlurper()
+import groovy.json.JsonSlurper
+
+def request = libraryResource 'com/devops/request.json' 
 // def result = slurper.parseText('{"name":"Harsh Verma","age":33,"pets":["dog","cat"]}')
-// // def result = slurper.parseText(request)
 def call(){
-def result = [
-  name:'Harsh Verma',
-  age:23
-]
+    def slurper = new JsonSlurper()
+    def result = slurper.parseText(request)
     println "Pipeline called !!!!"
     println Helper.logger("Custom message ${result.name}")
 }
