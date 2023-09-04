abb6dbc16c45fbdc5daa175ee3935a8ed5b20d12
harshmons <harshverma04111989@gmail.com>
2023-09-04 19:38:17 +0530

    Initial commit

--- src/com/enablement/Helper.groovy
+++ src/com/enablement/Helper.groovy
@@ -1,7 +1,8 @@
 package com.enablement
 
+import groovy.json.JsonSlurper
 // def request = libraryResource 'com/devops/request.json' 
-def slurper = new groovy.json.JsonSlurper()
+def slurper = new JsonSlurper()
 def result = slurper.parseText('{"name":"Harsh Verma","age":33,"pets":["dog","cat"]}')
 // def result = slurper.parseText(request)
 
