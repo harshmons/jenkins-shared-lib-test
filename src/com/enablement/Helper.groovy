7f60585fe6dcebc30410269c2d3633d4eaf4d918
harshmons <harshverma04111989@gmail.com>
2023-09-04 20:01:43 +0530

    Initial commit

--- src/com/enablement/Helper.groovy
+++ src/com/enablement/Helper.groovy
@@ -2,9 +2,6 @@ package com.enablement
 
 class Helper{
   public logger(msg){
-    def request = libraryResource 'com/devops/request.json' 
-    def slurper = new groovy.json.JsonSlurper()
-    def result = slurper.parseText(request)
-    return "Message from Logger helper method ${msg} Age - ${result.age}"
+    return "Message from Logger helper method ${msg}"
   }
 }
--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -4,6 +4,6 @@ def call(){
     def request = libraryResource 'com/devops/request.json' 
     def slurper = new groovy.json.JsonSlurper()
     def result = slurper.parseText(request)
-    println "Pipeline called !!!!"
-    println new Helper().logger("Custom message Name - ${result.name}")
+    println "Pipeline called !!!!!!!!"
+    println new Helper().logger("Custom message Name - ${result.name} Age ---- ${result.age}")
 }
